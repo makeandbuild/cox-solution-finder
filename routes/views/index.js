029a56399460364765b8520671df47e600794167
rnlambert7 <nlambert@maxmedia.com>
2015-01-16 17:36:29 -0500

    Navigation updates - removed industries and services views

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,26 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+
+	locals.data = {};
+
+	// ---------------
+	// Navigation Data
+	view.on('init', function(next) {
+		var q = keystone.list('Service').model.find().where('state', 'published');
+		q.exec(function(err, results) {
+			locals.data.navigation_services = results;
+			next(err);
+		});
+	});
+	view.on('init', function(next) {
+		var q = keystone.list('Industry').model.find().where('state', 'published');
+		q.exec(function(err, results) {
+			locals.data.navigation_industries = results;
+			next(err);
+		});
+	});
+	// ---------------
 	
 	// Render the view
 	view.render('index');
--- routes/views/industry.js
+++ routes/views/industry.js
@@ -40,6 +40,25 @@ exports = module.exports = function(req, res) {
 
 	});
 
+	// ---------------
+	// Navigation Data
+	view.on('init', function(next) {
+		var q = keystone.list('Service').model.find().where('state', 'published');
+		q.exec(function(err, results) {
+			locals.data.navigation_services = results;
+			next(err);
+		});
+	});
+	view.on('init', function(next) {
+		var q = keystone.list('Industry').model.find().where('state', 'published');
+		q.exec(function(err, results) {
+			locals.data.navigation_industries = results;
+			next(err);
+		});
+	});
+	// ---------------
+
+
 
 	// Render the view
 	view.render('industry');

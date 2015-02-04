8a2d7b5136379a771b507996d6d4f298106ec93c
rnlambert7 <nlambert@maxmedia.com>
2015-02-03 19:38:45 -0500

    Adjusted new svg styles for all previously png icon elements

--- models/Product.js
+++ models/Product.js
@@ -14,51 +14,21 @@ var Product = new keystone.List('Product', {
 
 var section = {
 	title: { type: String, requred: true },
-	icon_choice: {
-		type: Types.Boolean,
-		label: "Use Custom Icon?",
-		default: "false",
-	},
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
-		dependsOn: { icon_choice: [false, null, 0, '', '0', 'false', 'null'] },
 		options:
 			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice'
-	},
-	custom_icon: {
-		type: Types.S3File,
-		label: "Custom Icon",
-		dependsOn: { icon_choice: true },
-		s3path: 'uploads/images',
-		format: function(icon, file){
-			return '<pre>'+JSON.stringify(file, false, 2)+'</pre>'+'<img src="'+file.url+'" style="max-width: 300px">'
-		}
 	},
 	content: { type: Types.Textarea, height: 200 }
 }
 var resource = {
 	title: { type: String },
-	icon_choice: {
-		type: Types.Boolean,
-		label: "Use Custom Icon?",
-		default: "false",
-	},
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
-		dependsOn: { icon_choice: [false, null, 0, '', '0', 'false', 'null'] },
 		options:
 			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice'
-	},
-	custom_icon: {
-		type: Types.S3File,
-		label: "Custom Icon",
-		dependsOn: { icon_choice: true },
-		s3path: 'uploads/images',
-		format: function(icon, file){
-			return '<pre>'+JSON.stringify(file, false, 2)+'</pre>'+'<img src="'+file.url+'" style="max-width: 300px">'
-		}
 	},
 	resource_link: { type: String, initial:false },
 	description: { type: Types.Textarea, height: 100 }
@@ -73,11 +43,7 @@ Product.add({
 	hero: {
 		type: Types.S3File,
 		label: "Hero Image",
-		dependsOn: { icon_choice: true },
 		s3path: 'uploads/images',
-		format: function(icon, file){
-			return '<pre>'+JSON.stringify(file, false, 2)+'</pre>'+'<img src="'+file.url+'" style="max-width: 300px">'
-		}
 	},
 	resource_one: resource,
 	resource_two: resource,
--- models/Service.js
+++ models/Service.js
@@ -14,26 +14,11 @@ var Service = new keystone.List('Service', {
 
 var resource = {
 	title: { type: String },
-	icon_choice: {
-		type: Types.Boolean,
-		label: "Use Custom Icon?",
-		default: "false",
-	},
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
-		dependsOn: { icon_choice: [false, null, 0, '', '0', 'false', 'null'] },
 		options:
 			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice'
-	},
-	custom_icon: {
-		type: Types.S3File,
-		label: "Custom Icon",
-		dependsOn: { icon_choice: true },
-		s3path: 'uploads/images',
-		format: function(icon, file){
-			return '<pre>'+JSON.stringify(file, false, 2)+'</pre>'+'<img src="'+file.url+'" style="max-width: 300px">'
-		}
 	},
 	resource_link: { type: String, initial:false },
 	description: { type: Types.Textarea, height: 100 }
@@ -42,26 +27,11 @@ var resource = {
 Service.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	icon_choice: {
-		type: Types.Boolean,
-		label: "Use Custom Icon?",
-		default: "false",
-	},
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
-		dependsOn: { icon_choice: [false, null, 0, '', '0', 'false', 'null'] },
 		options:
 			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice'
-	},
-	custom_icon: {
-		type: Types.S3File,
-		label: "Custom Icon",
-		dependsOn: { icon_choice: true },
-		s3path: 'uploads/images',
-		format: function(icon, file){
-			return '<pre>'+JSON.stringify(file, false, 2)+'</pre>'+'<img src="'+file.url+'" style="max-width: 300px">'
-		}
 	},
 	heading: { type: String, required: true, default: 'Example Heading' },
 	content: { type: Types.Textarea, height: 400, required: true, default: 'Example Content' },

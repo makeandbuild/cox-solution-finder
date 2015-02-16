3237365c50a3ac813e7001045718fbb21b771819
Travis Warlick <twarlick@gmail.com>
2015-02-16 14:59:24 -0500

    remove user creation from deploy-setup.sh

--- script/deploy-setup.sh
+++ script/deploy-setup.sh
@@ -14,22 +14,6 @@ run() {
   ssh $APP_NAME@$TARGET_HOST "$cmd"
 }
 
-run_sudo() {
-  cmd="sudo"
-  for arg in "$@" ; do
-    cmd="$cmd $arg"
-  done
-  echo ">$cmd"
-  cmd="sh -c '$cmd'"
-  ssh $TARGET_HOST "$cmd"
-}
-
-###
-# Create user/app
-run_sudo useradd --home-dir $APP_ROOT --create-home --gid www $APP_NAME
-run_sudo chmod og+rx $APP_ROOT
-#TODO update $APP_ROOT/.ssh/authorized_keys
-
 ###
 # Initial setup
 run mkdir -p releases shared/tmp shared/log shared/config shared/system/uploads

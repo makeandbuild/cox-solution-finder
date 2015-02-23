9657f79ff47995868d0217fa0919c533e40584da
rnlambert7 <nlambert@maxmedia.com>
2015-02-22 22:20:51 -0500

    refactored JS files and split them up - refactored much of resources

---
+++ public/js/site/config.js
@@ -0,0 +1,17 @@
+// --------------------------------
+var config = {
+  "breakpoints" : {
+  	"site" : {
+  		"mobile" : 992,
+  	},
+  	"showroom" : {
+  		"tablet" : 1024,
+  		"touch" : 1920
+  	}
+  }
+}
+
+// Example Use-Case for functions in using breakpoints
+// if(document.documentElement.clientWidth <= config.breakpoints.site.mobile) {
+//     myFunctionHere();
+// }

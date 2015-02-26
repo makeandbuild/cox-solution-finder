d6ffe0b0923ede238e4febe46eaad8f0e49cce61
rnlambert7 <rnlambert7@gmail.com>
2015-02-25 22:58:41 -0500

    Connect for Companion Site

--- models/Connect.js
+++ models/Connect.js
@@ -15,8 +15,14 @@ var Connect = new keystone.List('Connect', {
 Connect.add({
 	name: { type: String, required: true, index: true, noedit: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', note: "This page will not show up unless published is chosen", index: true },
-	heading: { type: String, initial: false, label: "Industry Heading" },
-	content: { type: Types.Textarea, height: 400, initial: false, label: "Industry Content" }
+	heading: { type: String, initial: false, label: "Connect Page Heading" },
+	content: { type: Types.Textarea, height: 400, initial: false, label: "Connect Page Content" },
+	connect_background: {
+		type: Types.S3File,
+		label: 'Connect Background',
+		note: "Upload a 2X Resolution image. Image will be the background for the Connect Page.",
+		s3path: 'uploads/images'
+	},
 });
 
 Connect.register();

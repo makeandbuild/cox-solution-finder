46264fea0bd448877aa350fc0f827fdc8459fb2e
Jeremy Ciccone <jciccone@maxmedia.com>
2015-03-09 22:13:08 -0400

    first commit of the night

--- public/js/site/analytics.js
+++ public/js/site/analytics.js
@@ -1,7 +1,14 @@
 $(function() {
+	var deviceName = 'tablet';
+
+	if(document.documentElement.clientWidth >= config.breakpoints.site.showroom.touch) {
+		deviceName = 'touch';
+	}
+
 	var data = {
 		type: 'stats',
-		path: window.location.pathname
+		path: window.location.pathname,
+		device: deviceName
 	};
 
 	$.ajax({

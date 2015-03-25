e23509c020d163439cb7ba5961ce684fd9c43950
Jeremy Ciccone <jciccone@maxmedia.com>
2015-03-25 11:06:09 -0400

    reset password almost working

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 var keystone = require('keystone'),
 	Types = keystone.Field.Types,
-	client = nodeSES.createClient({ key: process.env.SES_KEY, secret: process.env.SES_SECRET });
+	nodeSES = require('node-ses'),
+	client = nodeSES.createClient({ key: process.env.SES_KEY, secret: process.env.SES_SECRET }),
+	sender = process.env.SES_SENDER,
+	hostname = process.env.STATIC_URI,
+	jade = require('jade');
 
 /**
  * User Model
@@ -12,7 +16,8 @@ var User = new keystone.List('User');
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true }
+	password: { type: Types.Password, initial: true, required: true },
+	resetPasswordKey: { type: String, hidden: true }
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 	isEditor: { type: Boolean, label: 'Can Edit Cox Solution Finder Content', index: true }
@@ -27,74 +32,66 @@ User.schema.virtual('canEdit').get(function() {
 	return this.isEditor;
 });
 
-User.schema.methods.resetPassword = function(callback) {
+// User.schema.methods.resetPassword = function(callback) {
 	
-	var user = this;
+// 	var user = this;
 	
-	user.resetPasswordKey = keystone.utils.randomString([16,24]);
+// 	user.resetPasswordKey = keystone.utils.randomString([16,24]);
 	
-	user.save(function(err) {
+// 	user.save(function(err) {
 		
-		if (err) return callback(err);
+// 		if (err) return callback(err);
 		
-		new keystone.Email('forgotten-password').send({
-			user: user,
-			link: '/reset-password/' + user.resetPasswordKey,
-			subject: 'Reset your Cox Solution Finder CMS Password',
-			to: user.email,
-			from: {
-				name: 'Jeremy Ciccone',
-				email: 'jciccone@maxmedia.com'
-			}
-		}, callback);
+// 		new keystone.Email('forgotten-password').send({
+// 			user: user,
+// 			link: '/reset-password/' + user.resetPasswordKey,
+// 			subject: 'Reset your Cox Solution Finder CMS Password',
+// 			to: user.email,
+// 			from: {
+// 				name: 'Jeremy Ciccone',
+// 				email: 'jciccone@maxmedia.com'
+// 			}
+// 		}, callback);
 		
-	});
+// 	});
 	
-}
+// }
 
-Enquiry.schema.methods.sendNotificationEmailSes = function(callback) {
+User.schema.methods.resetPassword = function(callback) {
+
+	var user = this;
+
+	user.resetPasswordKey = keystone.utils.randomString([16,24]);
+
 
 	if ('function' !== typeof callback) {
 		callback = function() {};
 	}
-	var enquiry = this;
 
-	var fs = require('fs');
+	user.save(function(err) {
 
-	//Render HTML template from EJS and send to User
-	fs.readFile('templates/emails/custom-email.ejs', 'utf-8', function(err, data) {
-	   if(err) {
-	   	console.log(err);
-	   	 callback(err);
-	   } else {
-   		var uid = enquiry._id.toString();
-   		console.log(uid);
-	   	var custom_url = process.env.PDOMAIN + '/personalized/' + security.encrypt(uid);
+		var html = jade.renderFile('templates/emails/forgotten-password.jade', {filename: 'templates/emails/forgotten-password.jade', 
+																				username: user.name.full, 
+																				host: hostname, 
+																				link: '/reset-password/' + user.resetPasswordKey
+																				});
+		client.sendemail({
+			to: user.email,
+	   		from: sender,
+	   		cc: '',
+			bcc: '',
+			subject: 'CMS EMAIL',
+			message: html
+		}, function (err, data, res) {
+			if(err) {
+				console.log(err);
+				callback(err);
+			} else {
+				console.log('SUCCESS!');
+				callback();
+			}
 
-		var html = EJS.render(data, {url: custom_url});
-
-			client.sendemail({
-				to: enquiry.email,
-		   		from: sender,
-		   		cc: '',
-				bcc: '',
-				subject: 'Thank you for your interest in Cox Business Solutions',
-				message: html
-			}, function (err, data, res) {
-				if(err) {
-					console.log(err);
-					callback(err);
-				} else {
-					console.log('SUCCESS!');
-					enquiry.is_notified = true;
-					callback();
-				}
-				// console.log('\x1b[36mData:\n\x1b[0m');
-				// console.log(data);
-			 	// 	console.log('\x1b[36mError:\n\x1b[0m');
-			 	// 	console.log(err);
-			});
-	   }
+		});
 	});
 
 };
@@ -104,5 +101,5 @@ Enquiry.schema.methods.sendNotificationEmailSes = function(callback) {
  * Registration
  */
 
-User.defaultColumns = 'name, email, isAdmin';
+User.defaultColumns = 'name, email, isAdmin', 'isEditor';
 User.register();

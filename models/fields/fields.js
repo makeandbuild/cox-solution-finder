0e26f23f36ea83d2da21b548afa2e9bcffe94d18
rnlambert7 <rnlambert7@gmail.com>
2015-03-27 12:05:45 -0400

    main models for industry complete

--- models/fields/fields.js
+++ models/fields/fields.js
@@ -31,17 +31,18 @@ exports.resource = function () {
 
 exports.media_buffet = function (section, types) {
 
-	function modalTitle(type) {
+	function modalTitle(type, dependsOnObj) {
+
 		return {
-			type: String,
+			type: Types.Html,
 			wysiwyg: true,
-			height:40,
+			height:10,
 			label: "Media "+type+": Title",
-			note: "Separate two blocks of text with ' | ' to create bolded text. <br>For example: normaltext | boldtext"
+			note: "PLACEHOLDER"
 		} 
 	};
 
-	function media_item(types) {
+	function media_item(types, m_cms_grouplabel, m_cms_groupname) {
 		var selected_types;
 		if (types) { selected_types = types; } else { selected_types = "None, Video, Image, Story"}
 
@@ -50,59 +51,64 @@ exports.media_buffet = function (section, types) {
 				type: Types.Select,
 				options: selected_types,
 				label: "Media Type",
-				note: "Choose"
+				note: "Choose",
+				m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 			},
 			thumbnail: {
 				type: Types.S3File,
 				label: 'Media Thumbnail: Preview Image',
 				note: "Upload a 2X Image to be the thumbnail image for the tile.",
-				s3path: 'uploads/images'
+				s3path: 'uploads/images',
+				m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 			},
 			video: {
-				dependsOn: { type: 'Video' },
 				title: modalTitle('Video'),
 				video: {
 					type: Types.S3File,
 					label: 'Media Video: File MP4',
 					note: 'MP4 Only. If no video is uploaded, video will default to the Homepage Hero Video.',
 					allowedTypes: ['video/mp4'],
-					s3path: 'uploads/videos'
+					s3path: 'uploads/videos',
+					m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 				},
 				video_webm: {
 					type: Types.S3File,
 					label: 'Media Video: File WebM',
 					note: 'WebM Only. If no WebM, video may function in all browsers. Also, if no MP4 video will not be shown.',
 					allowedTypes: ['video/webm'],
-					s3path: 'uploads/videos'
+					s3path: 'uploads/videos',
+					m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 				},
 				poster: {
 					type: Types.S3File,
 					label: 'Media Video: Poster on Video',
 					note: "Upload a 2X Image to be preview image for the video.",
-					s3path: 'uploads/images'
+					s3path: 'uploads/images',
+					m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 				}
 			},
 			image: {
-				dependsOn: { type: 'Image' },
 				type: Types.S3File,
 				label: 'Media Video: Preview Image',
 				note: "Upload a 2X Image to be preview image for the video.",
-				s3path: 'uploads/images'
+				s3path: 'uploads/images',
+				m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 			},
 			story: {
-				dependsOn: { type: 'Story' },
 				title: modalTitle('Story'),
 				content: { type: Types.Markdown,
 					wysiwyg: true,
 					height:300,
 					label: "Media Story: Content",
-					note: "No set limit for characters. Overflowed content will not be shown. All headers will be dark blue."
+					note: "No set limit for characters. Overflowed content will not be shown. All headers will be dark blue.",
+					m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 				},
 				featured_image: {
 					type: Types.S3File,
 					label: 'Media Story: Featured Image',
 					note: "Upload a 2X Image to be the Image located in the Article.",
-					s3path: 'uploads/images'
+					s3path: 'uploads/images',
+					m_cms: { group_label: m_cms_grouplabel, group_name: m_cms_groupname }
 				}
 			}
 		}
@@ -110,8 +116,7 @@ exports.media_buffet = function (section, types) {
 
 	return {
 
-		media_section_one: media_item("None, Video, Image, Story"),
-		media_section_two: media_item("None, Video, Image, Story"),
+		media_section_one: media_item("None, Video, Image, Story", "Media Section Top Left", "media_section_one"),
 		values: {
 			title: modalTitle('Values'),
 			content: { type: Types.Html,
@@ -129,7 +134,8 @@ exports.media_buffet = function (section, types) {
 				note: "Upload a 2X Image.",
 				s3path: 'uploads/images'
 			}
-		}
+		},
+		media_section_two: media_item("None, Video, Image, Story", "Media Section Bottom Right", "media_section_two")
 	};
 
 };

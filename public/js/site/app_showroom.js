18841196f989202c224f7849bd96bf2cd6c39f00
rnlambert7 <rnlambert7@gmail.com>
2015-05-14 10:53:34 -0400

    removed old code

--- public/js/site/app_showroom.js
+++ public/js/site/app_showroom.js
@@ -13,9 +13,7 @@ function initShowroom(){
 	itemNavigationAlternate($('.product-navigation-item'), $('.product-container'));
 	factoidGeneral();
 	if ($('.keyboard-container')[0]){
-		// jsKeyboard.init("virtualKeyboard");
 		connectKeyboard();
-		
 	}
 	inputClear();
 
@@ -37,38 +35,13 @@ function initShowroom(){
 	settingsActions();
 	settingsPageInits();
 
-	$( document ).ajaxSuccess(function( event, xhr, settings ) {
-	    //console.log(xhr.responseText);
-	});
-
-
 	$('.product-item-container').scroll(function(){
 		if ($(this).scrollTop() < 50){
 			$(this).children('.product-showroom-scroll').addClass('active');
 		} else {
 			$(this).children('.product-showroom-scroll').removeClass('active');
 		}
-	})
-	// Allows for Touchscreen Scrolling on Hover for the Products area of Services Views.
-	// $('.product-showroom-scroll').on('mouseover',function(){
-	// 	var parent = $(this).parent();
-	// 	var target = $(this);
-	// 	if(parent.hasClass('scrolled')){
-	// 		customScrollTo(parent, 0, 1000);
-
-	// 		target.fadeOut('300', function(){
-	// 			parent.removeClass('scrolled');
-	// 			target.fadeIn('300');
-	// 		});
-	// 	} else {
-	// 		customScrollTo(parent, parent.children('.row').last().offset().top, 1000);
-
-	// 		target.fadeOut('300', function(){
-	// 			parent.addClass('scrolled');
-	// 			target.fadeIn('300');
-	// 		});
-	// 	}
-	// });
+	});
 
 	// TV TABLE TRANSFER INIT CALL
 	tvTabletTransferInit();

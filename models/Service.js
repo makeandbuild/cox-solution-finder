d9a510e74b2af034a1e100978f886686f196836f
Taylor Martin <taylormartin.uga@gmail.com>
2016-05-11 16:16:13 -0400

    fixed the tiles with the new numbers of services and industries taken into account

--- models/Service.js
+++ models/Service.js
@@ -15,91 +15,91 @@ var Service = new keystone.List('Service', {
 });
 
 Service.add({
-	title: { 
-		type: String, 
-		required: true,	
-		m_cms: { 
+	title: {
+		type: String,
+		required: true,
+		m_cms: {
 			validations: {
 				required: true,
 				rangelength: [1,25]
 			},
-			showroom: true, 
+			showroom: true,
 			companion: true
 		}
 	},
-	state: { 
-		type: Types.Select, 
-		options: 'draft, published, archived', 
-		default: 'draft', 
-		note: "This page will not show up unless published is chosen", 
-		index: true 
+	state: {
+		type: Types.Select,
+		options: 'draft, published, archived',
+		default: 'draft',
+		note: "This page will not show up unless published is chosen",
+		index: true
 	},
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
 		options:
 			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice',
-		m_cms: { showroom: true, companion: true } 
+		m_cms: { showroom: true, companion: true }
 	},
 	heading: {
-		type: String, 
-		required: true, 
-		initial: false, 
+		type: String,
+		required: false,
+		initial: false,
 		label: "Service Heading",
-		m_cms: { 
+		m_cms: {
 			validations: {
 				required: true,
 				rangelength: [1,200]
 			},
-			showroom: true, 
-			companion: true 
+			showroom: true,
+			companion: true
 		}
 	},
 	content: {
-		type: Types.Textarea, 
-		height: 400, 
-		required: true, 
-		initial: false, 
+		type: Types.Textarea,
+		height: 400,
+		required: false,
+		initial: false,
 		label: "Service Content",
-		m_cms: { 
+		m_cms: {
 			validations: {
 				required: true,
 				rangelength: [1,1000]
 			},
-			showroom: true, 
+			showroom: true,
 			companion: true
-		} 
+		}
 	},
 	description: {
 		type: String,
-		required: true,
+		required: false,
 		initial: false,
 		label: "Service Description",
 		note: "Text will appear on other pages linking this industry, and not the industry page itself.",
-		m_cms: { 
+		m_cms: {
 			validations: {
 				required: true,
 				rangelength: [1,150]
 			},
-			showroom: true, 
+			showroom: true,
 			companion: true
-		} 
+		}
 	},
-	industries: { 
-		type: Types.Relationship, 
-		ref: 'Industry', 
-		required: true, 
-		many: true, 
-		initial: false, 
+	industries: {
+		type: Types.Relationship,
+		ref: 'Industry',
+		required: true,
+		many: true,
+		initial: false,
 		label: "Linked Industries",
 		note: "Select Industries to associate with this Service by dragging and dropping them. Relevant to showroom and companion.",
-		m_cms: { limit: 0, showroom: true, companion: true } 
+		m_cms: { limit: 0, showroom: true, companion: true }
 	},
 	resource_one: Fields.resource(),
 	resource_two: Fields.resource(),
 	resource_three: Fields.resource(),
 	editor: {
-		type: Types.Relationship, 
+		type: Types.Relationship,
 		ref: 'User'
 	},
 	checkoutTime: {

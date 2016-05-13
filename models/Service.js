f122aed5d56568bb4833ba77e5cfa1d635fa6ac9
gabriel arronte <gabrielarronte@gmail.com>
2016-05-13 13:09:14 -0400

    update variables

--- models/Service.js
+++ models/Service.js
@@ -37,8 +37,7 @@ Service.add({
 	svg_icon: {
 		type: Types.Select,
 		label: "Icon",
-		options:
-			'favorites_star, features_audio, features_building, features_channellisting, features_cloud, features_customization, features_data, features_features, features_mobility, features_networking, features_phone, features_protection, features_safety, features_scalable, features_service, features_signal, features_simple, features_speed, features_sports, features_wifi, industry_education, industry_government, industry_healthcare, industry_hospitality, industry_realestate, industry_residentialcommunities, partners_carriers, partners_resellers, resources_casestudy, resources_infographic, resources_whitepaper, service_internet, service_networking, service_TV, service_voice',
+		options:Fields.svg_icon()
 		m_cms: { showroom: true, companion: true }
 	},
 	heading: {
